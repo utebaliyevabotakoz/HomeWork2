f5281d898afce66620e5ff4163d7a962704a7e5f
Ботакоз Утебалиева <butebaliyeva@jmart.kz>
2022-11-20 22:24:37 +0600

    the3Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world! It's my HomeWork2");
+        System.out.println("Hello world! It's my HomeWork2. I have finished it");
+        System.out.println("Hey, guys");
     }
 }
